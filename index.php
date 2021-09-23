0404272c235e62524674c63386a992705a29ea10
Joao Chimello <joovitorch@gmail.com>
2021-09-23 19:46:22 -0300

    tipo int

--- index.php
+++ index.php
@@ -47,9 +47,13 @@
                     </ul>
                 </div>
                 <div class="modulo vermelho">
-                    <h3>Módulo 02</h3>
+                    <h3>Módulo 02 - Tipos</h3>
                     <ul>
-                        <li><a href="#">Exercício A</a></li>
+                        <li><a 
+                                href="exercicios.php?dir=tipos&file=int">
+                                Tipo Inteiro
+                            </a>
+                        </li>
                         <li><a href="#">Exercício B</a></li>
                         <li><a href="#">Exercício C</a></li>
                     </ul>

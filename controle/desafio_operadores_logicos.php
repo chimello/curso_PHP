dcbf6f87e022b4b583984c0915a8e48f3c5a593d
Joao Chimello <joovitorch@gmail.com>
2021-09-26 01:07:15 -0300

    Resposta desafio

--- controle/desafio_operadores_logicos.php
+++ controle/desafio_operadores_logicos.php
@@ -11,17 +11,17 @@
     <div>
         <label for="t1">Trabalho 1 (Terça):</label>
         <select name="t1" id="t1">
-            <option value="0">Selecione</option>
+            
             <option value="1">Executado</option>
-            <option value="2">Não Executado</option>
+            <option value="0">Não Executado</option>
         </select>
     </div>
     <div>
         <label for="t2">Trabalho 2 (Quinta):</label>
         <select name="t2" id="t2">
-            <option value="0">Selecione</option>
+            
             <option value="1">Executado</option>
-            <option value="2">Não Executado</option>
+            <option value="0">Não Executado</option>
         </select>
     </div>
     <button>Executar</button>
@@ -29,6 +29,8 @@
 
 <?php
 
+    //Minha Solução!
+    /*
     if(($_POST['t1'] == 1) && ($_POST['t2'] == 1)) {
         echo "Vamos comprar uma TV de 50' e vamos tomar Sorvete!!!<br>";
     } elseif ((($_POST['t1'] == 1) && ($_POST['t2'] == 2)) ||
@@ -37,10 +39,44 @@
     } elseif (($_POST['t1'] == 2) && ($_POST['t2'] == 2)) {
         echo "Vamos Ficar em casa!<br>";
     } elseif (($_POST['t1'] == 0) || ($_POST['t2'] == 0)) {
-        echo "Por favor selecione todas as opções!!!<br>";
+        echo "Por favor selecione todas as opções.<br>";
     } else {
         echo "Desconhecido!";
+    } */
+
+    //Solução professor!
+    if (isset($_POST['t1']) && isset($_POST['t2'])) {
+        $t1 = $_POST['t1'] === '1';
+    $t2 = !!$_POST['t2'];
+    $tv = '';
+    $sorvete = false;
+
+    if ($t1 && $t2) {
+        $tv = '50"';
+    }elseif ($t1 xor $t2) {
+        $tv = '32"';
+    };
+
+    if($t1 || $t2) {
+        $sorvete = true;
+    };
+
+    if ($tv) {
+        $resultado = "Vamos comprar uma TV de {$tv}";
+    } else {
+        $resultado = "Sem TV dessa vez :(";
     }
+
+    if (!$sorvete) {
+        $resultado .= '<br>Estamos mais saudáveis!';
+    } else {
+        $resultado .= '<br>Sorvete Liverado \o/';
+    }
+
+    echo "<p>$resultado</p>";
+    }
+
+    
 
 ?>
 

80a9024a9b44bb398dd195fcc91243403a5f4e40
chimello <49577324+chimello@users.noreply.github.com>
2021-12-23 14:04:54 -0300

    Login

--- login.php
+++ login.php
@@ -1,9 +1,3 @@
-<?php
-    session_start();
-    if(!$_SESSION['usuario']) {
-        header('Location: login.php');
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,15 +11,12 @@
 <body>
     <header class="cabecalho">
         <h1>Curso PHP</h1>
-        <h2>Índice dos Exercícios</h2>
+        <h2>Seja Bem Vindo</h2>
     </header>
-    <nav class="navegacao">
-        
-    </nav>
     <main class="principal">
         <div class="conteudo">
             <?php
-                require_once('menu.php');
+                
             ?>
         </div>
     </main>

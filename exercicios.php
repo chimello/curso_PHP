ecc09e432d6c27f0a94395fc7d6b889714e7009b
chimello <49577324+chimello@users.noreply.github.com>
2021-12-23 15:02:17 -0300

    Login - Logout Config arquivos/cores

--- exercicios.php
+++ exercicios.php
@@ -21,8 +21,10 @@
         <h2>Visualização dos Exercícios</h2>
     </header>
     <nav class="navegacao">
+    <span class="usuario">Usuário: <?= $_SESSION['usuario'] ?></span>
         <a href=<?="/{$_GET['dir']}/{$_GET['file']}.php"?> class="verde">Sem Formatação</a>
-        <a href="index.php" class="vermelho">Voltar</a>
+        <a href="index.php">Voltar</a>
+        <a href="logout.php" class="vermelho">Sair</a>
     </nav>
     <main class="principal">
         <div class="conteudo">
--- login.php
+++ login.php
@@ -1,3 +1,40 @@
+<?php session_start();
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    if($_POST['email']) {
+        $usuarios = [ 
+            [
+                "nome" => "Aluno Cod3r",
+                "email" => "alunos@cod3r.com.br",
+                "senha" => "123456",
+            ],
+            [
+                "nome" => "João Chimello",
+                "email" => "joao@email.com",
+                "senha" => "123",
+            ],
+        ];
+
+        foreach($usuarios as $usuario) {
+            $emailValido = $email === $usuario['email'];
+            $senhaValida = $senha === $usuario['senha'];
+
+            if($emailValido && $senhaValida) {
+                $_SESSION['erros'] = null;
+                $_SESSION['usuario'] = $usuario['nome'];
+                header('Location: index.php');
+            }
+        }
+
+        if(!$_SESSION['usuario']) {
+            $_SESSION['erros'] = ['Usuário e/ou Senha Inválidos!'];
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,18 +43,35 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/login.css">
     <title>Curso PHP</title>
 </head>
-<body>
+<body class="login">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Seja Bem Vindo</h2>
     </header>
     <main class="principal">
         <div class="conteudo">
-            <?php
-                
-            ?>
+            <h3>Identifique-se</h3>
+            <?php if($_SESSION['erros']): ?>
+                <div class="erros">
+                    <?php foreach ($_SESSION['erros'] as $erro): ?>
+                        <p> <?= $erro ?> </p>
+                    <?php endforeach ?>
+                </div>
+            <?php endif ?>
+            <form action="#" method="post">
+                <div>
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email">
+                </div>
+                <div>
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" id="senha">
+                </div>
+                <button>Entrar</button>
+            </form>
         </div>
     </main>
     <footer class="rodape">

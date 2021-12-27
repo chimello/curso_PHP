360219961b1ae493bae36d1a4af0426aad8150b2
chimello <49577324+chimello@users.noreply.github.com>
2021-12-27 15:10:30 -0300

    Formulário, primeira parte

--- formulario/formulario.php
+++ formulario/formulario.php
@@ -3,6 +3,49 @@
 <div class="titulo">Formulário PHP</div>
 
 <h2 class="h1" >Cadastro</h2>
+
+<?php
+
+    if(count($_POST) > 0) {
+        if(!filter_input(INPUT_POST, "nome")) {
+            echo "Nome é Obrigatório", '<br>';
+        }
+
+        //como o input é do tipo data, essa validação não será exibida
+        if(filter_input(INPUT_POST, "nascimento")) { 
+            $data = DateTime::createFromFormat('d/m/Y', $_POST['nascimento']);
+            if(!$data) {
+                echo "Data deve estar no formato: DD/MM/YYYY<br>";
+            }
+        }
+
+        if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            echo 'E-mail inválido!';
+        }
+        if(!filter_var($_POST['site'], FILTER_VALIDATE_URL)) {
+            echo 'Site inválido!<br>';
+        }
+
+        $filhosConfig = ["options"=> ["min_range" => 0, "max_range" => 20]];
+
+        if(!filter_var($_POST['filhos'], FILTER_VALIDATE_INT, $filhosConfig)
+                    && $_POST['filhos'] != ''
+                    && $_POST['filhos'] != 0) {
+            echo "Quantidade de filhos: {$_POST['filhos']} é inválida!";
+        }
+
+        $salarioConfig = ['options' => ['decimal' => ',']];
+
+        if(!filter_var($_POST['salario'], FILTER_VALIDATE_FLOAT, $salarioConfig)
+                    && $_POST['salario'] != ''
+                    && $_POST['salario'] != 0) {
+            echo "Salário: '{$_POST['salario']}' é inválido!";
+        }
+
+    }
+
+?>
+
 <form action="#" method="post">
     <div class="form-row">
         <div class="form-group col-md-9">
@@ -35,7 +78,7 @@
     <div class="form-row">
         <div class="form-group col-md-6">
             <label for="filhos">Quantidade de Filhos</label>
-            <input type="filhos" class="form-control"
+            <input type="number" class="form-control"
                 name="filhos" id="filhos" placeholder="Quantidade de Filhos"
                 value="<?= $_POST['filhos'] ?>">
         </div>
@@ -49,9 +92,3 @@
 
     <button class="btn btn-primary btn-lg">Enviar</button>
 </form>
-
-<?php
-
-
-
-?>
